9e22bed9d1a8c937ad020e4242c60ed2bd0f6219
ElisCB <ecardoso89@gmail.com>
2022-06-15 15:35:00 -0300

    kenzieflix finalizada

---
+++ OneDrive/Área de Trabalho/Kenzie/m1-s3-entrega-praticando-com-variaveis-ElisCB-master/script.js
@@ -0,0 +1,10 @@
+let nome = 'Elisângela';
+let sobrenome = 'Brito';
+let nomeCompleto = nome + " " + sobrenome
+let templateNomeCompleto = '${nome}  ${sobrenome}';
+let anoAtual = 2022;
+let anoNascimento = 1989;
+let idade = anoAtual - anoNascimento;
+let cidade = 'Vitória';
+let frase = "Meu nome é " + nomeCompleto + ", tenho "+idade+" anos e moro em "+cidade+"."
+
